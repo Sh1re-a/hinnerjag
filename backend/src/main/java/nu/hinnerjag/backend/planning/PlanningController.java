ae2bc1810f91a3746508c897073ee59c4efa4e1b
sh1re-a <Shirwac.abib@gmail.com>
2026-04-21 21:50:35 +0200

    feat: add planning controller for trip in trafiklab

---
+++ backend/src/main/java/nu/hinnerjag/backend/planning/PlanningController.java
@@ -0,0 +1,19 @@
+package nu.hinnerjag.backend.planning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlanningController {
+
+    private final PlanningService planningService;
+
+    public PlanningController(PlanningService planningService) {
+        this.planningService = planningService;
+    }
+
+    @GetMapping("/api/plans/test-trip")
+    public String testTrip() {
+        return planningService.fetchTestTrip();
+    }
+}
