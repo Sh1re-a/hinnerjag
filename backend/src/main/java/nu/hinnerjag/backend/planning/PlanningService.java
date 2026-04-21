c4ccf96a53147ddb897b5daf6e7d33b9f99233ab
sh1re-a <Shirwac.abib@gmail.com>
2026-04-21 21:49:16 +0200

    feat: add planning service with trafiklab journey request

---
+++ backend/src/main/java/nu/hinnerjag/backend/planning/PlanningService.java
@@ -0,0 +1,27 @@
+package nu.hinnerjag.backend.planning;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class PlanningService {
+
+    private final RestClient restClient = RestClient.create();
+
+    public String fetchTestTrip() {
+        String url = "https://journeyplanner.integration.sl.se/v2/trips" +
+                "?type_origin=coord" +
+                "&name_origin=18.082494:59.257932:WGS84[dd.ddddd]" +
+                "&type_destination=coord" +
+                "&name_destination=18.072226:59.319633:WGS84[dd.ddddd]" +
+                "&calc_number_of_trips=1" +
+                "&language=en" +
+                "&route_type=leasttime" +
+                "&gen_c=true";
+
+        return restClient.get()
+                .uri(url)
+                .retrieve()
+                .body(String.class);
+    }
+}
