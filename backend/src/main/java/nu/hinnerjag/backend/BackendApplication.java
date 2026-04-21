47f9641c37833ad1057be24d675564bbfad42a66
sh1re-a <Shirwac.abib@gmail.com>
2026-04-21 20:08:04 +0200

    feat: add health controller to backend

--- backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
+++ backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package dev.salt.backend;
+package nu.hinnerjag.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/nu/hinnerjag/backend/BackendApplicationTests.java
+++ backend/src/test/java/nu/hinnerjag/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.salt.backend;
+package nu.hinnerjag.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
