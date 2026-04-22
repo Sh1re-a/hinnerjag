9e25915b1cdd855084218329e44475129497daff
sh1re-a <Shirwac.abib@gmail.com>
2026-04-22 10:00:58 +0200

    refactor: extend dto with legs

--- backend/src/main/java/nu/hinnerjag/backend/external/trafiklab/dto/JourneyDto.java
+++ backend/src/main/java/nu/hinnerjag/backend/external/trafiklab/dto/JourneyDto.java
@@ -1,8 +1,11 @@
 package nu.hinnerjag.backend.external.trafiklab.dto;
 
+import java.util.List;
+
 public record JourneyDto(
         Integer tripDuration,
         Integer tripRtDuration,
-        Integer interchanges
+        Integer interchanges,
+        List<LegDto> legs
 ) {
 }
