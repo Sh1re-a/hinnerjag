598bc778ce00fc1e8eba0c7b9302c6b78f592740
sh1re-a <Shirwac.abib@gmail.com>
2026-04-22 12:50:11 +0200

    refactor: rename from orginName to boardingName

--- backend/src/main/java/nu/hinnerjag/backend/planning/dto/TripRouteResponse.java
+++ backend/src/main/java/nu/hinnerjag/backend/planning/dto/TripRouteResponse.java
@@ -6,7 +6,7 @@ public record TripRouteResponse(
         String mode,
         String line,
         String toward,
-        String originName,
+        String boardingName,
         String destinationName,
         String platform
 ) {
