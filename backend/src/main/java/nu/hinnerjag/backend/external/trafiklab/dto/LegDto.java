f646cb61c168307b7ba809d4b9b1964ca9e74937
sh1re-a <Shirwac.abib@gmail.com>
2026-04-22 13:17:31 +0200

    extend raw leg dto with duration

--- backend/src/main/java/nu/hinnerjag/backend/external/trafiklab/dto/LegDto.java
+++ backend/src/main/java/nu/hinnerjag/backend/external/trafiklab/dto/LegDto.java
@@ -3,6 +3,7 @@ package nu.hinnerjag.backend.external.trafiklab.dto;
 import java.util.List;
 
 public record LegDto(
+        Integer duration,
         PlaceDto origin,
         PlaceDto destination,
         TransportationDto transportation,
