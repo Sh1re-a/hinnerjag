c3f0321192f9c3a93cd768a78a461879d8fd8375
sh1re-a <Shirwac.abib@gmail.com>
2026-04-25 12:53:00 +0200

    feat: add Enable caching and enable retry in main application in backend

--- backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
+++ backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package nu.hinnerjag.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+@EnableRetry
 public class BackendApplication {
 
     public static void main(String[] args) {
