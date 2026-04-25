a659c004058d524000871b5cb1a4dd12951c1a83
sh1re-a <Shirwac.abib@gmail.com>
2026-04-25 15:06:48 +0200

    fix: remove retry från application

--- backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
+++ backend/src/main/java/nu/hinnerjag/backend/BackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@EnableRetry
 public class BackendApplication {
 
     public static void main(String[] args) {
